ff8a2615d4a3c992c3acb18658e9287c0a6acb24
Vivian Lim <vivlim@microsoft.com>
2023-09-26 14:17:33 -0700

    aarch64-linux: Update referenced compiler version
    also add a step to pipeline that lists available compilers, in case this guess is wrong.

--- buildTools/linux-arm64-cross.cmake
+++ buildTools/linux-arm64-cross.cmake
@@ -2,8 +2,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # which C and C++ complier to use
-set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc-9")
-set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++-9")
+set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc-11")
+set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++-11")
 
 # Where to look for the target environment. (More paths can be added here)
 set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
